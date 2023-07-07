63e226d48beb301fc8f2361401cc27e31c97e9f5
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-07-07 12:04:59 -0300

    feat: create list all tasks route

---
+++ app/Router/Routes/ListAllTasksRoute.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Router\Routes;
+
+class ListAllTasksRoute
+{
+  private $pdo;
+
+  public function __construct($pdo)
+  {
+    $this->pdo = $pdo;
+  }
+
+  public function getAllTasks()
+  {
+    $stmt = $this->pdo->query('SELECT * ' . 'FROM tasks');
+    $tasks = [];
+    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+      $tasks[] = [
+        'id' => $row['id'],
+        'project_name' => $row['project_name'],
+        'created_at' => $row['created_at'],
+        'updated_at' => $row['updated_at'],
+        'task' => $row['task'],
+        'is_finished' => $row['is_finished']
+      ];
+    }
+    return $tasks;
+  }
+}
