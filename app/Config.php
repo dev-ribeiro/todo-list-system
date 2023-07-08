1ebd367b0a0054ae54b97a4945c7ec0f1331177f
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-07-07 22:14:33 -0300

    config: include public path folder in config

--- app/Config.php
+++ app/Config.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
-class Config {
-   const PATH_TO_SQLITE_FILE = 'db/app.sqlite';
+class Config
+{
+  const PATH_TO_SQLITE_FILE = 'db/app.sqlite';
 
+  const PATH_TO_PUBLIC = 'public/';
 }
